9c538202aafeb7d7793cb02e6bb908893d1551ab
Abat Dauletbaev <abat.dauletbayev@gmail.com>
2022-10-07 01:54:53 +0500

    [frontend] replace next/script with regular script tag

--- apps/frontend/pages/_document.tsx
+++ apps/frontend/pages/_document.tsx
@@ -1,6 +1,5 @@
 import { createGetInitialProps } from '@mantine/next'
 import Document, { Head, Html, Main, NextScript } from 'next/document'
-import Script from 'next/script'
 
 const getInitialProps = createGetInitialProps()
 
@@ -15,7 +14,7 @@ export default class _Document extends Document {
           <Main />
           <NextScript />
 
-          <Script src={`https://www.google.com/recaptcha/api.js?render=${process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}`} />
+          <script defer src={`https://www.google.com/recaptcha/api.js?render=${process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}`} />
         </body>
       </Html>
     )
